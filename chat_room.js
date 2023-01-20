900c7ac296ed73c7cb0760ea402edd7812e1e24e
Revsri <117416155+Revsri@users.noreply.github.com>
2023-01-19 18:01:49 -0800

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAJmi3IVTg6-eCT5igpIBfIfo1o53S8tRQ",
+    authDomain: "letschatwebsite2.firebaseapp.com",
+    databaseURL: "https://letschatwebsite2-default-rtdb.firebaseio.com",
+    projectId: "letschatwebsite2",
+    storageBucket: "letschatwebsite2.appspot.com",
+    messagingSenderId: "898427209059",
+    appId: "1:898427209059:web:47ada43e9c7baace373f56"
+  };
+
+firebase.initializeApp(firebaseConfig);
